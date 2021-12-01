d55d3b9baf4d75dd12057cee5926ea5fa338d481
Tiago <ottiagos@gmail.com>
2021-12-01 20:20:54 -0300

    Readme e counter

--- src/components/hooks/useState/counter.component.jsx
+++ src/components/hooks/useState/counter.component.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
-import CounterStyles from './counter.styles'; 
+import CounterStyles from './counter.styles';
 
 const Counter = ({ initialCount }) => {
 
+    const sum10 = (value) => value + 10;
+
     // Um valor e uma função para atualizar esse valor
-    const [count, setCount] = useState(initialCount);
+    const [count, setCount] = useState(() => {
+        // Ideia útil caso o valor inicial seja algo demorado
+        // Essa função dentro de useState() só roda na primeira renderização
+        const initialValue = sum10(initialCount);
+        return initialValue;
+    });
 
     return (
         <CounterStyles>
             Count: {count}
-            <button onClick={() => setCount(initialCount)}>Reset</button>
+            <button onClick={() => setCount(sum10(initialCount))}>Reset</button>
+            {/* Prev tem o valor anterior do state  */}
             <button onClick={() => setCount(prev => prev + 1)}>+</button>
             <button onClick={() => setCount(prev => prev - 1)}>-</button>
         </CounterStyles>
